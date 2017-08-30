341c4170a60292c67a686742868e3ca4cc707061
Jibran Syed <jisyed14@gmail.com>
2017-08-30 10:59:53 -0500

    Using Catch's SECTIONs to enhance unit tests

--- Tests/FileIOSys_Test/FileTest.cpp
+++ Tests/FileIOSys_Test/FileTest.cpp
@@ -1,18 +1,71 @@
 #include "Tests\catch.hpp"
 #include "CCE\FileIOSys\File.hpp"
+#include <string>
 
 TEST_CASE("Opening a file at beginning", "[File]")
 {
 	FILE* testHandle = 0;
 
+	auto mode = cce::File::Mode::Read;
+	auto fileType = cce::File::Type::Text;
+	auto options = cce::File::Options::StartOfFile;
+	std::string fileName = std::string("NewData1.txt");
+
+	SECTION("Opening text file for reading")
 	{
-		cce::File file("NewData1.txt",
-					   cce::File::Mode::Read,
-					   cce::File::Type::Text,
-					   cce::File::Options::StartOfFile);
+		mode = cce::File::Mode::Read;
+		fileType = cce::File::Type::Text;
+		options = cce::File::Options::StartOfFile;
+		fileName = std::string("NewDataR.txt");
 	}
 
-	fopen_s(&testHandle, "NewData1.txt", "r");
+	SECTION("Opening text file for writing")
+	{
+		mode = cce::File::Mode::Write;
+		fileType = cce::File::Type::Text;
+		options = cce::File::Options::StartOfFile;
+		fileName = std::string("NewDataW.txt");
+	}
+
+	SECTION("Opening text file for read & write")
+	{
+		mode = cce::File::Mode::ReadAndWrite;
+		fileType = cce::File::Type::Text;
+		options = cce::File::Options::StartOfFile;
+		fileName = std::string("NewDataRW.txt");
+	}
+
+
+	SECTION("Opening binary file for reading")
+	{
+		mode = cce::File::Mode::Read;
+		fileType = cce::File::Type::Binary;
+		options = cce::File::Options::StartOfFile;
+		fileName = std::string("NewDataR.bin");
+	}
+
+	SECTION("Opening binary file for writing")
+	{
+		mode = cce::File::Mode::Write;
+		fileType = cce::File::Type::Binary;
+		options = cce::File::Options::StartOfFile;
+		fileName = std::string("NewDataW.bin");
+	}
+
+	SECTION("Opening binary file for read & write")
+	{
+		mode = cce::File::Mode::ReadAndWrite;
+		fileType = cce::File::Type::Binary;
+		options = cce::File::Options::StartOfFile;
+		fileName = std::string("NewDataRW.bin");
+	}
+
+
+	{
+		cce::File file(fileName.c_str(), mode, fileType, options);
+	}
+
+	fopen_s(&testHandle, fileName.c_str(), "r");
 	REQUIRE(testHandle != 0);
 	if (testHandle != 0)
 	{
